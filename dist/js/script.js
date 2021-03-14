81328ea25151f0e9079dacd360ab6b410d13b340
aljonnnnn <delumenaljon@gmail.com>
2021-03-14 16:39:21 +0800

    add content

---
+++ dist/js/script.js
@@ -0,0 +1,6 @@
+// $(document).ready(function(){
+    $('.nav__hamburger').click(function() {
+        $('.nav__hamburger').toggleClass('change')
+        $('.nav__menu').toggleClass('show')
+    })
+// });
